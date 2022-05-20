64b1989c63ae620c791942918308f437109a49d2
Masud Rana <63516873+masudrana03@users.noreply.github.com>
2022-05-20 16:17:03 +0600

    Day 2 70% done.

---
+++ Day-02/intro.js
@@ -0,0 +1,161 @@
+// Day 2
+
+// let nums = [1, 2, 3]
+// nums[0] = 10
+
+// console.log(nums)  // [10, 2, 3]
+
+// let nums = [1, 2, 3];
+// let numbers = [1, 2, 3];
+
+// console.log(nums == numbers)  // false
+
+
+// // js math object.
+
+// const PI = Math.PI
+
+// console.log(PI)                            // 3.141592653589793
+
+// // Rounding to the closest number
+// // if above .5 up if less 0.5 down rounding
+
+// console.log(Math.round(PI))                // 3 to round values to the nearest number
+
+// console.log(Math.round(9.81))              // 10
+
+// console.log(Math.floor(PI))                // 3 rounding down
+
+// console.log(Math.ceil(PI))                 // 4 rounding up
+
+// console.log(Math.min(-5, 3, 20, 4, 5, 10)) // -5, returns the minimum value
+
+// console.log(Math.max(-5, 3, 20, 4, 5, 10)) // 20, returns the maximum value
+
+// const randNum = Math.random() // creates random number between 0 to 0.999999
+// console.log(randNum)
+
+// // Let us  create random number between 0 to 10
+
+// const num = Math.floor(Math.random () * 11) // creates random number between 0 and 10
+// console.log(num)
+
+// //Absolute value
+// console.log(Math.abs(-10))      // 10
+
+// //Square root
+// console.log(Math.sqrt(100))     // 10
+
+// console.log(Math.sqrt(2))       // 1.4142135623730951
+
+// // Power
+// console.log(Math.pow(3, 2))     // 9
+
+// console.log(Math.E)             // 2.718
+
+// // Logarithm
+// // Returns the natural logarithm with base E of x, Math.log(x)
+// console.log(Math.log(2))        // 0.6931471805599453
+// console.log(Math.log(10))       // 2.302585092994046
+
+// // Returns the natural logarithm of 2 and 10 respectively
+// console.log(Math.LN2)           // 0.6931471805599453
+// console.log(Math.LN10)          // 2.302585092994046
+
+// // Trigonometry
+// Math.sin(0)
+// Math.sin(60)
+
+// Math.cos(0)
+// Math.cos(60)
+
+
+
+// let randomNum = Math.random() // generates 0 to 0.999...
+
+
+// console.log('I hope everyone is enjoying the 30 Days Of JavaScript challenge.\nDo you ?') // line break
+// console.log('Days\tTopics\tExercises')
+// console.log('Day 1\t3\t5')
+// console.log('Day 2\t3\t5')
+// console.log('Day 3\t3\t5')
+// console.log('Day 4\t3\t5')
+// console.log('This is a backslash  symbol (\\)') // To write a backslash
+// console.log('In every programming language it starts with \"Hello, World!\"')
+// console.log("In every programming language it starts with \'Hello, World!\'")
+// console.log('The saying \'Seeing is Believing\' isn\'t correct in 2020')
+
+
+// let js = 'JavaScript'
+// console.log(js.length)         // 10
+// let firstName = 'Asabeneh'
+// console.log(firstName.length)  // 8
+
+// let string = 'JavaScript'
+// console.log(string.substr(2,5))    // Script
+
+
+// let string = 'JavaScript'
+
+// console.log(string.substring(0,4))     // Java
+// console.log(string.substring(4,10))    // Script
+// console.log(string.substring(4))       // Script
+
+// let country = 'Finland'
+
+// console.log(country.substring(0, 3))   // Fin
+// console.log(country.substring(3, 7))   // land
+// console.log(country.substring(3))      // land
+
+
+
+// let string = '30 Days Of JavaScript'
+
+// console.log(string.split())     // Changes to an array -> ["30 Days Of JavaScript"]
+// console.log(string.split(' '))  // Split to an array at space -> ["30", "Days", "Of", "JavaScript"]
+
+// let firstName = 'Asabeneh'
+
+// console.log(firstName.split())    // Change to an array - > ["Asabeneh"]
+// console.log(firstName.split(''))  // Split to an array at each letter ->  ["A", "s", "a", "b", "e", "n", "e", "h"]
+
+// let countries = 'Finland, Sweden, Norway, Denmark, and Iceland'
+
+// console.log(countries.split(','))  // split to any array at comma -> ["Finland", " Sweden", " Norway", " Denmark", " and Iceland"]
+// console.log(countries.split(', ')) //  ["Finland", "Sweden", "Norway", "Denmark", "and Iceland"]
+
+
+let string = '30 Days Of JavaScript'
+
+console.log(string.indexOf('D'))          // 3
+console.log(string.indexOf('Days'))       // 3
+console.log(string.indexOf('days'))       // -1
+console.log(string.indexOf('a'))          // 4
+console.log(string.indexOf('JavaScript')) // 11
+console.log(string.indexOf('Script'))     //15
+console.log(string.indexOf('script'))     // -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
